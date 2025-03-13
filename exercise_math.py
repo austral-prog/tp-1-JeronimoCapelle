c2c8250217460f8e3b4bd1404c4569cd7c60d276
¨JeronimoCapelle¨ <jcapelle@mail.austral.edu.ar>
2025-03-13 17:16:48 -0300

    removed unnecessary debug print statements

--- exercise_math.py
+++ exercise_math.py
@@ -2,25 +2,12 @@ def math():
     a = 57
     b = 7
 
-    print("La suma:")
     print(a + b)
-    
-    print("La diferencia:")
     print(a - b)
-    
-    print("El producto:")
     print(a*b)
-    
-    print("El promedio:")
     print((a+b)/2)
-    
-    print("El cociente entero:")
     print(a//b)
-    
-    print("El resto de la division entera:")
     print(a%b)
-    
-    print("El valor real de la division:")
     print(a/b)
     
 math()
